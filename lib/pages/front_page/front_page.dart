097b1955315a1f68d0968472b01c3f7a7f3762f4
Dominik <dominik.franciszek.ilski@gmail.com>
2020-05-19 17:20:11 +0200

    changed titleControlller becouse of animation reasons, buttons change opacity now based of main animation frame

--- lib/pages/front_page/front_page.dart
+++ lib/pages/front_page/front_page.dart
@@ -1,10 +1,11 @@
+import 'package:flare_flutter/flare.dart';
+import 'package:flare_flutter/flare_controller.dart';
 import 'package:flutter/material.dart';
-import 'package:avalon/shared/widgets/main_button.dart';
 
+
+import '../../shared/widgets/main_button.dart';
 
 import 'widgets/title.dart' as ragnarok_title;
-
-
 
 /// public
 class FrontPage extends StatefulWidget {
@@ -16,6 +17,17 @@ class FrontPage extends StatefulWidget {
 
 class _FrontPageState extends State<FrontPage> {
   void tester() => print('button pressed');
+  static final _openingAnimation = 'OpeningAnimation';
+  static final _animationDuration = Duration(milliseconds: 1000);
+  static var _visable = false;
+
+  double visability() => _visable ? 1.0 : 0.0;
+
+  void _startButtonsAnimation() {
+    setState(() {
+      _visable = true;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,15 +35,26 @@ class _FrontPageState extends State<FrontPage> {
       body: Stack(
         fit: StackFit.expand,
         children: <Widget>[
-          ragnarok_title.Title(),
+          ragnarok_title.Title(
+            ragnarok_title.TitleController(_openingAnimation,
+                buttonAnimation: _startButtonsAnimation),
+          ),
           Align(
-            alignment: Alignment(0,0.8),
+            alignment: Alignment(0, 0.8),
             child: Column(
               mainAxisSize: MainAxisSize.min,
               mainAxisAlignment: MainAxisAlignment.end,
               children: <Widget>[
-                MainButton('New Game', tester),
-                MainButton('Join Game', tester)
+                AnimatedOpacity(
+                  opacity: visability(),
+                  duration: _animationDuration,
+                  child: MainButton('New Game', tester),
+                ),
+                AnimatedOpacity(
+                  opacity: visability(),
+                  duration: _animationDuration,
+                  child: MainButton('Join Game', tester),
+                ),
               ],
             ),
           )
@@ -41,3 +64,6 @@ class _FrontPageState extends State<FrontPage> {
     );
   }
 }
+
+///
+
--- lib/pages/front_page/widgets/title.dart
+++ lib/pages/front_page/widgets/title.dart
@@ -8,34 +8,34 @@ import 'package:flare_flutter/flare_controls.dart';
 ///public title
 class Title extends StatelessWidget {
   static final _openingAnimation = 'OpeningAnimation';
-  static final _titleAnimation = 'Title';
-
-  final _controls = TitleController(_openingAnimation, _titleAnimation);
+  final FlareController _controller;
+  ///
+  Title(this._controller) ;
+  
 
   @override
   Widget build(BuildContext context) {
     return FlareActor(
-      'assets/animations/ragnarok_main.flr',
+      'assets/animations/ragnarok_main1.flr',
       alignment: Alignment(0, -1),
       animation: _openingAnimation,
       snapToEnd: true,
       fit: BoxFit.fitWidth,
-      controller: _controls,
+      controller: _controller,
     );
   }
 }
 
 class TitleController extends FlareController {
   ///
-  final String _animation;
-  final String _secondAnimation;
-
+  VoidCallback buttonAnimation;
+  var _animationStatut = true;
   var _duration = 0.0;
-
+  final String _animation;
   ActorAnimation _actor;
 
   ///
-  TitleController(this._animation, this._secondAnimation);
+  TitleController(this._animation, {@required this.buttonAnimation});
 
   @override
   void initialize(FlutterActorArtboard artBoard) {
@@ -47,9 +47,16 @@ class TitleController extends FlareController {
     _duration += elapsed;
     if (_duration >= _actor.duration) {
       _actor.apply(_actor.duration, artboard, 1);
-      //_actor = artboard.getAnimation(_secondAnimation);
       return false;
     }
+
+    if(_duration >= 2.5){
+      if (_animationStatut) {
+          buttonAnimation();
+          _animationStatut = false;
+        }
+    }
+    
     _duration %= _actor.duration;
     return true;
   }
@@ -59,3 +66,4 @@ class TitleController extends FlareController {
     // TODO: implement setViewTransform
   }
 }
+
